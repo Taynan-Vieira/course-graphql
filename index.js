8ef1cfe0eb4e8810bdf239b390cbfa8bf18d6d3f
Taynan Vieira <taynan.vieira.one@gmail.com>
2022-10-25 16:41:54 -0300

    aula-6-schema-e-query|object-types|fields

--- index.js
+++ index.js
@@ -1,43 +1,70 @@
-const {gql, ApolloServer} = require("apollo-server")
+const { gql, ApolloServer } = require("apollo-server");
+
+const produtos = [
+  {
+    id: 1,
+    nome: "Notebook",
+    valor: 12000.0,
+  },
+  {
+    id: 2,
+    nome: "TV",
+    valor: 6000.0,
+  },
+];
+
+const usuarios = [
+  {
+    id: 1,
+    nome: "Paulo",
+    salario: 10000.0,
+    ativo: true,
+    idade: 29,
+  },
+  {
+    id: 2,
+    nome: "João",
+    salario: 15000.0,
+    ativo: false,
+    idade: 35,
+  },
+];
 
 const typeDefs = gql`
-    type Query{
-        idade: Int
-        salario: Float
-        nome: String
-        ativo: Boolean
-        id: ID
-        tecnologias: [String!]!
-        
-    }
+  type Usuario {
+    idade: Int
+    salario: Float
+    nome: String
+    ativo: Boolean
+    id: ID
+  }
+
+  type Produto {
+    id: ID
+    nome: String
+    valor: Float
+  }
+
+  type Query {
+    usuarios: [Usuario]
+    produtos: [Produto]
+  }
 `;
 
 const resolvers = {
-    Query: {
-        idade(){
-            return 13;
-        },
-        salario(){
-            return 156843.56;
-        },
-        nome(){
-            return "GraphQL";
-        },
-        ativo(){
-            return true;
-        },
-        id(){
-            return 12345678;
-        },
-        tecnologias(){
-            return ['CSS', 'JavaScript', 'SQL'];
-        }
-    }
+  Query: {
+    usuarios() {
+      return usuarios;
+    },
+    produtos() {
+      return produtos;
+    },
+  },
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
-server.listen()
+server.listen();
